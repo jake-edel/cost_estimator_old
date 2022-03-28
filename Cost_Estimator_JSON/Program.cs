637481f8ca85f78e8295fdca2259313e85268d96
Jake <Jake@DESKTOP-9DECGN5>
2022-03-28 10:27:00 -0600

    Add project files.

---
+++ Cost_Estimator_JSON/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using JUST;
+using System.IO;
+
+namespace JUST.Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = File.ReadAllText("/Users/Jake/source/repos/Cost_Estimator/Cost_Estimator_JSON/data/stair_configuration.json");
+
+            string transformer = File.ReadAllText("/Users/Jake/source/repos/Cost_Estimator/Cost_Estimator_JSON/data/transformer.json"); ;
+
+            string transformedString = JsonTransformer.Transform(transformer, input);
+
+            Console.WriteLine(transformedString);
+        }
+    }
+}
