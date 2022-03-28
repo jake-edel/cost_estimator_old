364a4c55eda226864cf03cb9a1e2f804d6405755
Jakob Edelstein <jakobedelstein@gmail.com>
2022-03-28 17:05:01 -0600

    test 2 throws type error

--- Cost_Estimator_JSON/Program.cs
+++ Cost_Estimator_JSON/Program.cs
@@ -16,6 +16,13 @@ namespace JUST.Test
 
             string transformer = File.ReadAllText("/Users/Jake/source/repos/Cost_Estimator/Cost_Estimator_JSON/data/landing_transformer.json"); ;
 
+
+            //JUSTContext context = new JUSTContext
+            //{
+            //    EvaluationMode = EvaluationMode.FallbackToDefault,
+
+            //};
+
             string transformedString = JsonTransformer.Transform(transformer, input);
 
             Console.WriteLine(transformedString);
